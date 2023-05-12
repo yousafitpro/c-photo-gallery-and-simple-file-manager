e9680d4e8a37a34ca7a8ef25184d6e56ee0e76d4
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-05-12 18:19:23 +0500

    ok

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,10 +15,10 @@ Route::prefix('Gallery')
     ->namespace('Yousafitpro\PhotoLibrary\Controllers')
     ->group(function () {
         Route::get('/', 'UcPhotoLibraryController@index')->name('uc_gallery.index');
-        Route::post('/upload_file', ['UcPhotoLibraryController@upload_file'])->name('uc_gallery.upload_file');
-        Route::get('/gallery-widget', ['UcPhotoLibraryController@gallery_widget'])->name('uc_gallery.gallery_widget');
-        Route::get('/gallery-delete-file/{id}', [ 'UcPhotoLibraryController@gallery_delete_file'])->name('uc_gallery.gallery_delete_file');
-        Route::get('/gallery-widget-search', ['UcPhotoLibraryController@gallery_widget_search'])->name('uc_gallery.gallery_widget_search');
+        Route::post('/upload_file', 'UcPhotoLibraryController@upload_file')->name('uc_gallery.upload_file');
+        Route::get('/gallery-widget', 'UcPhotoLibraryController@gallery_widget')->name('uc_gallery.gallery_widget');
+        Route::get('/gallery-delete-file/{id}',  'UcPhotoLibraryController@gallery_delete_file')->name('uc_gallery.gallery_delete_file');
+        Route::get('/gallery-widget-search', 'UcPhotoLibraryController@gallery_widget_search')->name('uc_gallery.gallery_widget_search');
 ///adasd
     });
 
