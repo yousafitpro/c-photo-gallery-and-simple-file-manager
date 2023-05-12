f4d873b0acacaf35739b64d01182e66cedd9336d
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-05-12 19:05:04 +0500

    ok

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,6 +12,7 @@
 //adasd
 //asdasd
 Route::prefix('Gallery')
+    ->middleware('auth')
     ->namespace('Yousafitpro\PhotoLibrary\Controllers')
     ->group(function () {
         Route::get('/', 'UcPhotoLibraryController@index')->name('uc_gallery.index');
